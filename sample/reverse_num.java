af4ef0af0e3cb21ca79fa9b3fea90e80390bf046
Jatin Gidh <134076892+Jatin0410@users.noreply.github.com>
2023-12-06 18:37:54 +0530

    commit

---
+++ sample/reverse_num.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int reverse(int x) {
+        int reverse = 0;
+
+        while (x != 0) {
+            int lastDigit = x % 10;
+
+            // Check for overflow/underflow
+            if (reverse > Integer.MAX_VALUE / 10 || reverse < Integer.MIN_VALUE / 10) {
+                return 0;
+            }
+
+            reverse = reverse * 10 + lastDigit;
+            x /= 10;
+        }
+
+        return reverse;
+    }
+}
