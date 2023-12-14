ecfbe7c86548758b5e05cfb35e29b98aed3eab14
Jatin Gidh <134076892+Jatin0410@users.noreply.github.com>
2023-12-14 22:45:54 +0530

    commit

--- array/27_Remove_Elements.java
+++ array/27_Remove_Elements.java
@@ -6,7 +6,7 @@ If nums[i] is not equal to val, it means it is a non-target element.
 Set nums[index] to nums[i] to store the non-target element at the current index position.
 Increment index by 1 to move to the next position for the next non-target element.
 Continue this process until all elements in the array have been processed.
-Finally, return the value of index, which represents the length of the modified array. */
+Finally, return the value of index, which represents the length of the modified array */
 
 class Solution {
     public int removeElement(int[] nums, int val) {
